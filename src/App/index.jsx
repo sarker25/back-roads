23763af895d33c249fa5a5720e64934f4ce7127f
sarker25 <shahadathbinoy25@outlook.com>
2023-08-09 18:08:19 +0600

    all components created

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,5 +1,20 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import About from "../components/About";
+import Service from "../components/Service";
+import Tour from "../components/Tour";
+import Footer from "../components/Footer";
 const App = () => {
-  return <>App</>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Service />
+      <Tour />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
